30582764f6094d42d9042f33e4a07644901e0902
Yusent Chig <yusent@protonmail.com>
2017-09-11 07:45:56 -0700

    Move invoice xml to a file

--- test/unit/CFDI/ParserSpec.hs
+++ test/unit/CFDI/ParserSpec.hs
@@ -1,72 +1,18 @@
-module CFDI.ParserSpec where
+module CFDI.ParserSpec (spec) where
 
+import CFDI.Parser
+import CFDI.Types
 import Data.Maybe          (fromJust, isJust, isNothing)
 import Data.Time.Calendar  (Day(ModifiedJulianDay))
 import Data.Time.LocalTime (LocalTime(..), TimeOfDay(..))
 import Test.Hspec
-import CFDI.Parser
-import CFDI.Types
-
-invoiceCFDI :: CFDI
-invoiceCFDI =
-  fromJust $ parseCFDI invoiceXML
-
-invoiceXML :: String
-invoiceXML =
-  " <?xml version='1.0' encoding='UTF-8'?>                                     \
-  \ <cfdi:Comprobante total='536.216' NumCtaPago='1212'                        \
-  \   subTotal='1161.07' tipoDeComprobante='ingreso' Moneda='MXN'              \
-  \   sello='SOMENOTSORANDOMSIGNATURE'                                         \
-  \   certificado='SOMENOTSORANDOMCERTIFICATE' serie='A'                       \
-  \   formaDePago='PAGO EN UNA SOLA EXHIBICION' version='3.2'                  \
-  \   fecha='2017-06-12T12:00:00' condicionesDePago='CREDITO 15 DIAS'          \
-  \   metodoDePago='03' noCertificado='00001000001212121212'                   \
-  \   LugarExpedicion='Issued In' folio='144144'>                                 \
-  \   <cfdi:Emisor rfc='XAXX010101000' nombre='Issuer Name'>                   \
-  \     <cfdi:DomicilioFiscal colonia='Fiscal Suburb'                          \
-  \       calle='Fiscal Street' noExterior='Fiscal External Number'            \
-  \       municipio='Fiscal Municipality' estado='Fiscal State'                \
-  \       pais='Fiscal Country' codigoPostal='Fiscal Zip Code'/>               \
-  \     <cfdi:RegimenFiscal Regimen='Fiscal Regime'/>                          \
-  \   </cfdi:Emisor>                                                           \
-  \   <cfdi:Receptor rfc='XEXX010101000' nombre='Receipient Name'>             \
-  \     <cfdi:Domicilio calle='Recipient Street'                               \
-  \       municipio='Recipient Municipality' estado='Recipient State'          \
-  \       pais='Recipient Country' codigoPostal='Recipient Zip Code'/>         \
-  \   </cfdi:Receptor>                                                         \
-  \   <cfdi:Conceptos>                                                         \
-  \     <cfdi:Concepto noIdentificacion='Product 1 ID' importe='112.00'        \
-  \       valorUnitario='112.00' unidad='Product 1 Unit' cantidad='1.00'       \
-  \       descripcion='Product 1 Description'>                                 \
-  \       <cfdi:InformacionAduanera numero='Product 1 Import Number'           \
-  \         fecha='1986-08-22' aduana='Product 1 Custom'/>                     \
-  \     </cfdi:Concepto>                                                       \
-  \     <cfdi:Concepto noIdentificacion='Product 2 ID' importe='424.24'        \
-  \       valorUnitario='212.12' unidad='Product 2 Unit' cantidad='2.00'       \
-  \       descripcion='Product 2 Description'>                                 \
-  \     </cfdi:Concepto>                                                       \
-  \   </cfdi:Conceptos>                                                        \
-  \   <cfdi:Impuestos totalImpuestosRetenidos='12.144'                         \
-  \     totalImpuestosTrasladados='12.12'>                                     \
-  \     <cfdi:Traslados>                                                       \
-  \       <cfdi:Traslado importe='12.12' tasa='16' impuesto='IVA'/>            \
-  \     </cfdi:Traslados>                                                      \
-  \     <cfdi:Retenciones>                                                     \
-  \       <cfdi:Retencion importe='12.144' impuesto='ISR'/>                    \
-  \     </cfdi:Retenciones>                                                    \
-  \   </cfdi:Impuestos>                                                        \
-  \   <cfdi:Complemento>                                                       \
-  \     <tfd:TimbreFiscalDigital UUID='12121212-1212-1212-1212-121212121212'   \
-  \       FechaTimbrado='2015-11-23T23:05:00' version='1.0'                    \
-  \       selloCFD='Signature' noCertificadoSAT='SAT Certificate Number'       \
-  \       selloSAT='SAT Signature'/>                                           \
-  \   </cfdi:Complemento>                                                      \
-  \ </cfdi:Comprobante>                                                        "
 
 spec :: Spec
 spec = do
   describe "CFDI.Parser.parseCFDI" $ do
     context "when parsing an invoice" $ do
+      Just invoiceCFDI <- parseCFDI <$> runIO (readFile "test/xml/invoice.xml")
+
       it "parses account number" $ do
         accountNumber invoiceCFDI `shouldBe` Just "1212"
 
