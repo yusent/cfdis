59a1d2b31565738d5ab6eb73a75b53c4cf69560f
Yusent Chig <yusent@protonmail.com>
2017-09-12 15:12:36 -0700

    Add Chainable instances for basic types

--- src/CFDI/Chain.hs
+++ src/CFDI/Chain.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module CFDI.Chain where
 
 import CFDI
+import Data.Time.Calendar  (Day, showGregorian)
+import Data.Time.Format    (defaultTimeLocale, formatTime)
+import Data.Time.LocalTime (LocalTime)
 
 class Chainable a where
   addToChain :: a -> String -> String
@@ -11,3 +16,18 @@ class Chainable a where
       chain' = chain x
 
   chain :: a -> String
+
+instance Chainable Day where
+  chain = showGregorian
+
+instance Chainable Float where
+  chain = show
+
+instance Chainable LocalTime where
+  chain = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
+
+instance Chainable a => Chainable (Maybe a) where
+  chain = maybe "" chain
+
+instance Chainable String where
+  chain = id
