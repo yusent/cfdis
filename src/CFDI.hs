2d79877605a64a3ff4a63c63207a945909a92143
Yusent Chig <yusent@protonmail.com>
2017-09-21 09:23:33 -0700

    Add stamp function to CFDI module

--- src/CFDI.hs
+++ src/CFDI.hs
@@ -4,10 +4,18 @@ module CFDI where
 
 import CFDI.Chain           (chain)
 import CFDI.CSD             (signWithCSD)
-import CFDI.Parser
+import CFDI.PAC             (PAC, StampError, getPacStamp)
+import CFDI.Parser          (ParseError(MalformedXML), Parsed, parseCFDI)
 import CFDI.Renderer        (render)
-import CFDI.Types           (CFDI, signature)
+import CFDI.Types
+  ( CFDI
+  , Complement(..)
+  , complement
+  , pacStamp
+  , signature
+  )
 import Control.Error.Safe   (justErr)
+import Data.Maybe           (fromMaybe)
 import Data.Text            (Text, append, pack)
 import Text.XML.Light       (parseXMLDoc, ppTopElement)
 import Text.XML.Light.Lexer (XmlSource)
@@ -23,6 +31,14 @@ signCFDIWith csdPemPath cfdi =
   fmap (fmap addSignatureToCFDI) . signWithCSD csdPemPath $ originalChain cfdi
   where
     addSignatureToCFDI sig = cfdi { signature = sig }
+
+stamp :: PAC p => CFDI -> p -> IO (Either StampError CFDI)
+stamp cfdi =
+  fmap (fmap addStampToCFDI) . getPacStamp (toXML cfdi)
+  where
+    addStampToCFDI stamp' =
+      cfdi { complement = Just (complement' { pacStamp = Just stamp' }) }
+    complement' = fromMaybe (Complement Nothing) $ complement cfdi
 
 toXML :: CFDI -> Text
 toXML = pack . ppTopElement . render
