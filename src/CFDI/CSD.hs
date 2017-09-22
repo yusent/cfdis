7fc33efbcae09c990a1cdb9c60b7c703729908eb
Yusent Chig <yusent@protonmail.com>
2017-09-22 08:23:06 -0700

    Refactor CFDI.CSD functions

--- src/CFDI/CSD.hs
+++ src/CFDI/CSD.hs
@@ -1,9 +1,14 @@
-module CFDI.CSD where
+module CFDI.CSD
+  ( CsdCerData(..)
+  , getCsdCerData
+  , csdKeyToPem
+  , signWithCSD
+  ) where
 
-import Data.ByteString           (empty)
+import Data.ByteString           (ByteString)
 import Data.ByteString.Base64    (encode)
 import Data.Maybe                (fromJust)
-import Data.Text                 (Text, concat, pack, split, unpack)
+import Data.Text                 (Text, concat, empty, pack, split, unpack)
 import Data.Text.Encoding        (decodeUtf8, encodeUtf8)
 import Data.Time.LocalTime       (LocalTime)
 import Data.Time.Format          (defaultTimeLocale, parseTimeM)
@@ -21,68 +26,56 @@ data CsdCerData = CsdCerData
 -- this I couldn't figure out how to use it.
 
 getCsdCerData :: FilePath -> IO (Either Text CsdCerData)
-getCsdCerData cerPath = do
-  pemOrErr <- getPem
-
-  case pemOrErr of
-    Right pem -> do
-      serialOrErr <- getSerial $ encodeUtf8 pem
-
-      case serialOrErr of
-        Right serial -> do
-          endDateOrErr <- getEndDate $ encodeUtf8 pem
-
-          case endDateOrErr of
-            Right endDate -> do
-              return . Right . CsdCerData endDate serial $ sha1 pem
-
-            Left err -> return $ Left err
-
-        Left err -> return $ Left err
-
-    Left err -> return $ Left err
+getCsdCerData cerPath =
+  getPem cerPath >>= eitherErrOrContinue (\pem ->
+    getSerial pem >>= eitherErrOrContinue (\serial ->
+      getEndDate pem >>= eitherErrOrContinue (\endDate ->
+        return . Right . CsdCerData endDate serial $ sha1 pem)))
   where
-    getPem = do
-      let args = ["x509", "-inform", "DER", "-outform", "PEM", "-in", cerPath]
-      getResult <$> readProcessWithExitCode "openssl" args empty
-
-    getSerial pem = do
-      let args = ["x509", "-noout", "-serial"]
-      parseSerial . getResult <$> readProcessWithExitCode "openssl" args pem
-      where
-        parseSerial = fmap (pack . odds . unpack . head . tail . split (== '='))
-        odds [] = []
-        odds [x] = []
-        odds (e1 : e2 : xs) = e2 : odds xs
-
-    getEndDate pem = do
-      let args = ["x509", "-noout", "-enddate"]
-      parseEndDate . getResult <$> readProcessWithExitCode "openssl" args pem
-      where
-        parseEndDate = fmap (fromJust . parseTimeM True defaultTimeLocale format . unpack)
-        format = "notAfter=%b %d %H:%M:%S %Y %Z"
-
-    getResult (exitCode, stdout, stderr) =
-      case exitCode of
-        ExitSuccess   -> Right $ decodeUtf8 stdout
-        ExitFailure _ -> Left  $ decodeUtf8 stderr
-
+    eitherErrOrContinue = either (return . Left)
     sha1 = concat . init . init . tail . split (== '\n')
 
 csdKeyToPem :: FilePath -> String -> IO (Either Text Text)
-csdKeyToPem keyPath keyPass = do
-  let pass = "pass:" ++ keyPass
-  let args = ["pkcs8", "-inform", "DER", "-in", keyPath, "-passin", pass]
-  (exitCode, stdout, stderr) <- readProcessWithExitCode "openssl" args empty
-  return $ case exitCode of
-    ExitSuccess   -> Right $ decodeUtf8 stdout
-    ExitFailure _ -> Left  $ decodeUtf8 stderr
+csdKeyToPem keyPath keyPass =
+  runOpenSSL ["pkcs8", "-inform", "DER", "-in", keyPath, "-passin", pass] empty
+  where
+    pass = "pass:" ++ keyPass
 
 signWithCSD :: FilePath -> Text -> IO (Either Text Text)
-signWithCSD csdPemPath txt = do
-  let args = ["dgst", "-sha1", "-sign", csdPemPath]
-  (exitCode, stdout, stderr) <-
-    readProcessWithExitCode "openssl" args $ encodeUtf8 txt
+signWithCSD csdPemPath =
+  fmap (either (Left . decodeUtf8) (Right . decodeUtf8 . encode))
+    . runOpenSSL_ ["dgst", "-sha1", "-sign", csdPemPath]
+
+-- Helpers
+
+getPem :: FilePath -> IO (Either Text Text)
+getPem cerPath =
+  runOpenSSL ["x509", "-inform", "DER", "-outform", "PEM", "-in", cerPath] empty
+
+getSerial :: Text -> IO (Either Text Text)
+getSerial pem =
+  parseSerial <$> runOpenSSL ["x509", "-noout", "-serial"] pem
+  where
+    parseSerial = fmap (pack . odds . unpack . head . tail . split (== '='))
+    odds [] = []
+    odds [x] = []
+    odds (e1 : e2 : xs) = e2 : odds xs
+
+getEndDate :: Text -> IO (Either Text LocalTime)
+getEndDate pem =
+  fmap parseEndDate <$> runOpenSSL ["x509", "-noout", "-enddate"] pem
+  where
+    parseEndDate = fromJust . parseTimeM True defaultTimeLocale format . unpack
+    format = "notAfter=%b %d %H:%M:%S %Y %Z"
+
+runOpenSSL :: [String] -> Text -> IO (Either Text Text)
+runOpenSSL args =
+  fmap (either (Left . decodeUtf8) (Right . decodeUtf8)) . runOpenSSL_ args
+
+runOpenSSL_ :: [String] -> Text -> IO (Either ByteString ByteString)
+runOpenSSL_ args stdin = do
+  let bsStdin = encodeUtf8 stdin
+  (exitCode, stdout, stderr) <- readProcessWithExitCode "openssl" args bsStdin
   return $ case exitCode of
-    ExitSuccess   -> Right . decodeUtf8 $ encode stdout
-    ExitFailure _ -> Left $ decodeUtf8 stderr
+    ExitSuccess   -> Right stdout
+    ExitFailure _ -> Left  stderr
