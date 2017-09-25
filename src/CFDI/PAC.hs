571c42cc52212bd3005ffb1576897a2ebd857b48
Yusent Chig <yusent@protonmail.com>
2017-09-25 07:11:07 -0700

    Add first PAC (ITimbre)

--- src/CFDI/PAC.hs
+++ src/CFDI/PAC.hs
@@ -1,23 +1,38 @@
 module CFDI.PAC where
 
-import CFDI.Types    (CFDI, Complement(..), PacStamp, complement, pacStamp)
+import CFDI.Parser   (ParseError)
 import CFDI.Renderer (toXML)
+import CFDI.Types    (CFDI, Complement(..), PacStamp, complement, pacStamp)
 import Data.Maybe    (fromMaybe)
 import Data.Text     (Text)
 
-type XML = Text
-
 class PAC p where
-  getPacStamp :: XML -> p -> IO (Either StampError PacStamp)
+  getPacStamp :: CFDI -> p -> IO (Either StampError PacStamp)
 
-data StampError = StampError
-  { stampErrMsg  :: Text
-  , stampErrCode :: Maybe Text
-  }
+data StampError
+  = PacConnectionError
+    { connErrMsg :: Text
+    }
+  | PacError
+    { pacErrMsg  :: Text
+    , pacErrCode :: Maybe Text
+    }
+  | PacHTTPError
+    { pacHTTPCode :: Int
+    , pacHTTPBody :: Text
+    }
+  | PacStampNotPresent
+  | ParsePacResponseError
+    { parsePacErrMsg :: Text
+    }
+  | ParsePacResponseXMLError
+    { parseError :: ParseError
+    }
+  deriving (Eq, Show)
 
 stamp :: PAC p => CFDI -> p -> IO (Either StampError CFDI)
 stamp cfdi =
-  fmap (fmap addStampToCFDI) . getPacStamp (toXML cfdi)
+  fmap (fmap addStampToCFDI) . getPacStamp cfdi
   where
     addStampToCFDI stamp' =
       cfdi { complement = Just (complement' { pacStamp = Just stamp' }) }
