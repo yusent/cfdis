408161176e8b5b4afef5be2b1b1ea753d2de4081
Yusent Chig <yusent@protonmail.com>
2017-09-28 16:22:02 -0700

    Add relationship types catalog

--- src/CFDI/Catalogs.hs
+++ src/CFDI/Catalogs.hs
@@ -230,6 +230,34 @@ instance Catalog PaymentMethod where
   toCode OneTimePayment = "PUE"
   toCode PartialPayment = "PPD"
 
+data RelationshipType
+  = CreditNote
+  | DebitNote
+  | ReturnedGoods
+  | Substitution
+  | PrevInvoicedTransfer
+  | PrevTransferedInvoice
+  | AdvanceApplication
+  deriving (Eq, Show)
+
+instance Catalog RelationshipType where
+  fromCode "01" = Just CreditNote
+  fromCode "02" = Just DebitNote
+  fromCode "03" = Just ReturnedGoods
+  fromCode "04" = Just Substitution
+  fromCode "05" = Just PrevInvoicedTransfer
+  fromCode "06" = Just PrevTransferedInvoice
+  fromCode "07" = Just AdvanceApplication
+  fromCode _    = Nothing
+
+  toCode CreditNote            = "01"
+  toCode DebitNote             = "02"
+  toCode ReturnedGoods         = "03"
+  toCode Substitution          = "04"
+  toCode PrevInvoicedTransfer  = "05"
+  toCode PrevTransferedInvoice = "06"
+  toCode AdvanceApplication    = "07"
+
 data WayToPay
   = Cash
   | NominalCheck
