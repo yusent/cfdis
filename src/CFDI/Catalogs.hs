c2fdc3032e1bfe1f5a83f5afeed6ce95822c9f97
Yusent Chig <yusent@protonmail.com>
2017-09-29 11:46:50 -0700

    Add measurement units catalog

--- src/CFDI/Catalogs.hs
+++ src/CFDI/Catalogs.hs
@@ -2,6 +2,7 @@ module CFDI.Catalogs
   ( module CFDI.Catalogs.CfdiTypes
   , module CFDI.Catalogs.Countries
   , module CFDI.Catalogs.Currencies
+  , module CFDI.Catalogs.MeasurementUnits
   , module CFDI.Catalogs.PaymentMethods
   , module CFDI.Catalogs.ProductOrServices
   , module CFDI.Catalogs.RelationshipTypes
@@ -14,6 +15,7 @@ module CFDI.Catalogs
 import CFDI.Catalogs.CfdiTypes
 import CFDI.Catalogs.Countries
 import CFDI.Catalogs.Currencies
+import CFDI.Catalogs.MeasurementUnits
 import CFDI.Catalogs.PaymentMethods
 import CFDI.Catalogs.ProductOrServices
 import CFDI.Catalogs.RelationshipTypes
