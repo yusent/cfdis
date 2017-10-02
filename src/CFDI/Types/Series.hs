a0bed0c01beafd601f44451b8bfc165d04da61c1
Yusent Chig <yusent@protonmail.com>
2017-10-02 11:41:22 -0700

    Use regexes in Series and Folio parsing

--- src/CFDI/Types/Series.hs
+++ src/CFDI/Types/Series.hs
@@ -1,15 +1,17 @@
 module CFDI.Types.Series where
 
 import CFDI.Types.Type
-import Data.Text       (Text, pack, unpack)
+import Data.Text        (Text, pack, unpack)
+import Text.Regex       (mkRegex)
+import Text.Regex.Posix (matchTest)
 
 data Series = Series Text deriving (Eq, Show)
 
 instance Type Series where
   parseExpr str
-    | l > 0 && l < 26 = Right . Series $ pack str
+    | matchTest regExp str = Right . Series $ pack str
     | otherwise = Left $ DoesNotMatchExpr "[^|]{1,25}"
     where
-      l = length str
+      regExp = mkRegex "^.{1,25}$"
 
   render (Series s) = unpack s
