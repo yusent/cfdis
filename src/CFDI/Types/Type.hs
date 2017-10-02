d47e5b191ee2c6d9377a8505d5b9a6233fc6093a
Yusent Chig <yusent@protonmail.com>
2017-10-02 08:32:09 -0700

    Add Type instances for LocalTime and Text

--- src/CFDI/Types/Type.hs
+++ src/CFDI/Types/Type.hs
@@ -1,5 +1,10 @@
 module CFDI.Types.Type where
 
+import Control.Error.Safe  (justErr)
+import Data.Text           (Text, pack, unpack)
+import Data.Time.LocalTime (LocalTime)
+import Data.Time.Format    (defaultTimeLocale, formatTime, parseTimeM)
+
 data ParseError
   = InvalidValue
   | DoesNotMatchExpr Text
@@ -10,3 +15,17 @@ class Type t where
   parse :: String -> Either ParseError t
 
   render :: t -> String
+
+instance Type LocalTime where
+  parse = justErr (DoesNotMatchExpr expr)
+        . parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
+    where
+      expr = "(20[1-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(([01]\
+             \[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])"
+
+  render = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
+
+instance Type Text where
+  parse = Right . pack
+
+  render = unpack
