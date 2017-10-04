6101b4321cd342adc5ab7cfe624cc979a1af48cd
Yusent Chig <yusent@protonmail.com>
2017-10-04 16:13:03 -0700

    Add toXML function to CFDI module

--- src/CFDI.hs
+++ src/CFDI.hs
@@ -2,12 +2,13 @@ module CFDI
   ( module CFDI.Types.CFDI
   , parseCfdiFile
   , parseCfdiXml
+  , toXML
   ) where
 
 import CFDI.Types.CFDI      (CFDI)
-import CFDI.XmlNode         (XmlParseError(MalformedXML), parseNode)
+import CFDI.XmlNode         (XmlParseError(MalformedXML), parseNode, renderNode)
 import Control.Error.Safe   (justErr)
-import Text.XML.Light       (parseXMLDoc)
+import Text.XML.Light       (parseXMLDoc, ppTopElement)
 import Text.XML.Light.Lexer (XmlSource)
 
 parseCfdiFile :: FilePath -> IO (Either XmlParseError CFDI)
@@ -16,3 +17,6 @@ parseCfdiFile fp = parseCfdiXml <$> readFile fp
 parseCfdiXml :: XmlSource s => s -> Either XmlParseError CFDI
 parseCfdiXml xmlSource = justErr MalformedXML (parseXMLDoc xmlSource)
                      >>= parseNode
+
+toXML :: CFDI -> String
+toXML = ppTopElement . renderNode
