3484c0e4ee2a079e8b4f6d12014a6344410b821b
Yusent Chig <yusent@protonmail.com>
2018-06-26 15:04:20 -0700

    Receive XML from stdin and pac name as an argument

--- stamp/main.hs
+++ stamp/main.hs
@@ -1,73 +1,79 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import CFDI (parseCfdiFile, ppXmlParseError, signWith, toXML)
+import CFDI (parseCfdiXml, ppXmlParseError, signWith, toXML)
 import CFDI.PAC (ppStampError, stampWithRetry)
 import CFDI.PAC.Fel (Fel(Fel), FelEnv(FelProductionEnv))
 import CFDI.PAC.ITimbre (ITimbre(ITimbre), ITimbreEnv(Production))
+import Data.ByteString (hGetContents)
 import Data.Char (toLower)
 import Data.Text (pack, unpack)
 import System.Environment (getArgs, getEnv)
 import System.Exit (ExitCode(ExitFailure), exitWith)
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPutStrLn, stderr, stdin)
 
 main :: IO ()
 main = do
-  (cfdiPath : csdPemPath : _) <- getArgs
-  eitherErrOrParsedCfdi <- parseCfdiFile cfdiPath
+  args <- getArgs
 
-  case eitherErrOrParsedCfdi of
-    Left parseErr -> do
-      hPutStrLn stderr $ ppXmlParseError "  " parseErr
-      exitWith $ ExitFailure 2
+  case args of
+    (csdPemPath : pacName : _) -> do
+      eitherErrOrParsedCfdi <- parseCfdiXml <$> hGetContents stdin
 
-    Right parsedCfdi -> do
-      eitherErrorOrSignedCfdi <- signWith csdPemPath parsedCfdi
-
-      case eitherErrorOrSignedCfdi of
-        Left signErr -> do
-          hPutStrLn stderr $ unpack signErr
+      case eitherErrOrParsedCfdi of
+        Left parseErr -> do
+          hPutStrLn stderr $ ppXmlParseError "  " parseErr
           exitWith $ ExitFailure 3
 
-        Right signedCfdi -> do
-          pacName <- map toLower <$> getEnv "STAMP_PAC"
+        Right parsedCfdi -> do
+          eitherErrorOrSignedCfdi <- signWith csdPemPath parsedCfdi
 
-          case pacName of
-            "itimbre" -> do
-              user <- getEnv "STAMP_ITIMBRE_USER"
-              pass <- getEnv "STAMP_ITIMBRE_PASS"
-              rfc <- getEnv "STAMP_ITIMBRE_RFC"
-
-              let pac =
-                    ITimbre (pack user) (pack pass) (pack rfc) "" "" Production
-
-              eitherErrOrStampedCfdi <- stampWithRetry signedCfdi pac
-
-              case eitherErrOrStampedCfdi of
-                Left stampErr -> do
-                  hPutStrLn stderr $ ppStampError stampErr
-                  exitWith $ ExitFailure 5
-
-                Right stampedCfdi -> do
-                  putStrLn $ toXML stampedCfdi
-
-            "fel" -> do
-              user <- getEnv "STAMP_FEL_USER"
-              pass <- getEnv "STAMP_FEL_PASS"
-              rfc <- getEnv "STAMP_FEL_RFC"
-
-              let pac =
-                    Fel (pack user) (pack pass) (pack rfc) "" "" FelProductionEnv
-
-              eitherErrOrStampedCfdi <- stampWithRetry signedCfdi pac
-
-              case eitherErrOrStampedCfdi of
-                Left stampErr -> do
-                  hPutStrLn stderr $ ppStampError stampErr
-                  exitWith $ ExitFailure 5
-
-                Right stampedCfdi -> do
-                  putStrLn $ toXML stampedCfdi
-
-            unknownPac -> do
-              hPutStrLn stderr $  "Unknown PAC " ++ unknownPac
+          case eitherErrorOrSignedCfdi of
+            Left signErr -> do
+              hPutStrLn stderr $ unpack signErr
               exitWith $ ExitFailure 4
+
+            Right signedCfdi -> do
+              case map toLower pacName of
+                "itimbre" -> do
+                  user <- getEnv "STAMP_ITIMBRE_USER"
+                  pass <- getEnv "STAMP_ITIMBRE_PASS"
+                  rfc <- getEnv "STAMP_ITIMBRE_RFC"
+
+                  let pac =
+                        ITimbre (pack user) (pack pass) (pack rfc) "" "" Production
+
+                  eitherErrOrStampedCfdi <- stampWithRetry signedCfdi pac
+
+                  case eitherErrOrStampedCfdi of
+                    Left stampErr -> do
+                      hPutStrLn stderr $ ppStampError stampErr
+                      exitWith $ ExitFailure 6
+
+                    Right stampedCfdi -> do
+                      putStrLn $ toXML stampedCfdi
+
+                "fel" -> do
+                  user <- getEnv "STAMP_FEL_USER"
+                  pass <- getEnv "STAMP_FEL_PASS"
+                  rfc <- getEnv "STAMP_FEL_RFC"
+
+                  let pac =
+                        Fel (pack user) (pack pass) (pack rfc) "" "" FelProductionEnv
+
+                  eitherErrOrStampedCfdi <- stampWithRetry signedCfdi pac
+
+                  case eitherErrOrStampedCfdi of
+                    Left stampErr -> do
+                      hPutStrLn stderr $ ppStampError stampErr
+                      exitWith $ ExitFailure 6
+
+                    Right stampedCfdi -> do
+                      putStrLn $ toXML stampedCfdi
+
+                unknownPac -> do
+                  hPutStrLn stderr $  "Unknown PAC " ++ unknownPac
+                  exitWith $ ExitFailure 5
+
+    _ -> do
+      hPutStrLn stderr $ "Usage: stamp [Path to CSD PEM file] [PAC name]"
+      exitWith $ ExitFailure 2
