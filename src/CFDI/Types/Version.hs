aa43ce614cefbf14c1b74576eaa0b9ebd05c86d8
Yusent Chig <yusent@protonmail.com>
2022-05-22 11:29:59 -0700

    Accept 4.0 version

--- src/CFDI/Types/Version.hs
+++ src/CFDI/Types/Version.hs
@@ -9,7 +9,9 @@ instance Chainable Version where
   chain (Version v) = chain v
 
 instance Type Version where
+  parseExpr "4.0" = Right $ Version 4.0
   parseExpr "3.3" = Right $ Version 3.3
   parseExpr e     = Left  $ InvalidValue e
 
+  render (Version 4.0) = "4.0"
   render _ = "3.3"
