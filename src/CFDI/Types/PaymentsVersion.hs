8c222311c4121964cab2c7b30ce3e068dc6616bf
Yusent Chig <yusent@protonmail.com>
2022-05-22 11:30:57 -0700

    Accept 2.0 version in Payments complement

--- src/CFDI/Types/PaymentsVersion.hs
+++ src/CFDI/Types/PaymentsVersion.hs
@@ -9,7 +9,9 @@ instance Chainable PaymentsVersion where
   chain (PaymentsVersion v) = chain v
 
 instance Type PaymentsVersion where
+  parseExpr "2.0" = Right $ PaymentsVersion 2.0
   parseExpr "1.0" = Right $ PaymentsVersion 1.0
   parseExpr e     = Left  $ InvalidValue e
 
+  render (PaymentsVersion 2.0) = "2.0"
   render _ = "1.0"
