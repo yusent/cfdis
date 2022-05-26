8a10ce2fe4c0330b8e51ac4695b4bee4e167d483
Yusent Chig <yusent@protonmail.com>
2022-05-26 11:36:12 -0700

    Fix typo in pagos xsd

--- src/CFDI/Types/CFDI.hs
+++ src/CFDI/Types/CFDI.hs
@@ -86,7 +86,7 @@ instance XmlNode CFDI where
         ("http://www.sat.gob.mx/cfd/3 \
          \http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv33.xsd \
          \http://www.sat.gob.mx/Pagos \
-         \http://www.sat.gob.mx/sitio_internet/cfd/Pagos/Pagos10.xs \
+         \http://www.sat.gob.mx/sitio_internet/cfd/Pagos/Pagos10.xsd \
          \http://www.sat.gob.mx/TimbreFiscalDigital \
          \http://www.sat.gob.mx/sitio_internet/cfd/\
          \TimbreFiscalDigital/TimbreFiscalDigitalv11.xsd" :: Text)
